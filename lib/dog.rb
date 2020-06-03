95b9af0846b6d2d316b01c46d612924cbcc108a2
Taevon Jordan <nude.plus@Taevons-MBP.home>
2020-06-03 18:50:02 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
-# Add your code here
+require 'pry'
+
+
+class Dog
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.map do |dogs|
+      puts dogs.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+end
+
+# ball = Dog.new("Ball")
+# terry = Dog.new("Terry")
+# april = Dog.new("April")
